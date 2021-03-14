24e34c38eb81b725dfcf77e8ffa2302b09cdb28b
chenaoh <cdhenao@gmail.com>
2021-03-14 14:58:19 -0500

    Se agrega el mensaje en el main

--- src/clases/Principal.java
+++ src/clases/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
 
 	
 	public static void main(String arg[]) {
+		System.out.println("Este es un mensaje nuevo");
 		VentanaPrincipal miVentana=new VentanaPrincipal();
 		miVentana.setVisible(true);
 	}
